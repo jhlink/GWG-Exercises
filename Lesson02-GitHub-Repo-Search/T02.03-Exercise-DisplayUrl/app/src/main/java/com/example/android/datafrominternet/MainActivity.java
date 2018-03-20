0fb50812351a3c4b4260c0adfa7abbe4d9fc2090
James Kim <jhlink@users.noreply.github.com>
2018-03-20 05:22:24 -0400

    task: Implement makeGithubSearchQuery

--- Lesson02-GitHub-Repo-Search/T02.03-Exercise-DisplayUrl/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ Lesson02-GitHub-Repo-Search/T02.03-Exercise-DisplayUrl/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -24,6 +24,10 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.android.datafrominternet.utilities.NetworkUtils;
+
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
     private EditText mSearchBoxEditText;
@@ -43,8 +47,11 @@ public class MainActivity extends AppCompatActivity {
         mSearchResultsTextView = (TextView) findViewById(R.id.tv_github_search_results_json);
     }
 
-    // TODO (2) Create a method called makeGithubSearchQuery
-    // TODO (3) Within this method, build the URL with the text from the EditText and set the built URL to the TextView
+    void makeGithubSearchQuery() {
+        String urlQueryFromEditText = mSearchBoxEditText.getText().toString();
+        URL finishedURL = NetworkUtils.buildUrl(urlQueryFromEditText);
+        mUrlDisplayTextView.setText(finishedURL.toString());
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
