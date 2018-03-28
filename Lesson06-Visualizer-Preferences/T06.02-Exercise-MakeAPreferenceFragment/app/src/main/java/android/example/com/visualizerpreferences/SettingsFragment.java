2b9192041d00620ae7d0524420fe72e6b4f18809
James Kim <jhlink@users.noreply.github.com>
2018-03-28 04:36:58 -0400

    task[2]: Create SettingsFragment class

---
+++ Lesson06-Visualizer-Preferences/T06.02-Exercise-MakeAPreferenceFragment/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
@@ -0,0 +1,6 @@
+package android.example.com.visualizerpreferences;
+
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+}
