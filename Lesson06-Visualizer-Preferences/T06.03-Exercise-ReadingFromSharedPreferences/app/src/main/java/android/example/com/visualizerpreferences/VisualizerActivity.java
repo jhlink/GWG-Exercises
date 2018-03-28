6e85c97b67b6c40e3495ba8a837809a442414028
James Kim <jhlink@users.noreply.github.com>
2018-03-28 05:21:14 -0400

    task[1-3]: Implement sharedPreferences and get value of show_bass from SharedPreferences object

--- Lesson06-Visualizer-Preferences/T06.03-Exercise-ReadingFromSharedPreferences/app/src/main/java/android/example/com/visualizerpreferences/VisualizerActivity.java
+++ Lesson06-Visualizer-Preferences/T06.03-Exercise-ReadingFromSharedPreferences/app/src/main/java/android/example/com/visualizerpreferences/VisualizerActivity.java
@@ -18,11 +18,13 @@ package android.example.com.visualizerpreferences;
 
 import android.Manifest;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.example.com.visualizerpreferences.AudioVisuals.AudioInputReader;
 import android.example.com.visualizerpreferences.AudioVisuals.VisualizerView;
 import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -48,9 +50,15 @@ public class VisualizerActivity extends AppCompatActivity {
 
     // COMP (1) Change the name of default setup to setupSharedPreferences
     private void setupSharedPreferences() {
-        // TODO (2) Get a reference to the default shared preferences from the PreferenceManager class
-        // TODO (3) Get the value of the show_bass checkbox preference and use it to call setShowBass
-        mVisualizerView.setShowBass(true);
+        // COMP (2) Get a reference to the default shared preferences from the PreferenceManager
+        // class
+        // COMP (3) Get the value of the show_bass checkbox preference and use it to call
+        // setShowBass
+
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean resultingBool = sharedPrefs.getBoolean("show_bass", true);
+
+        mVisualizerView.setShowBass(resultingBool);
         mVisualizerView.setShowMid(true);
         mVisualizerView.setShowTreble(true);
         mVisualizerView.setMinSizeScale(1);
