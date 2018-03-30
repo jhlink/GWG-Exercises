29910ca213e24ac8a7a0bdb99cc34e0e5e5e64e1
James Kim <jhlink@users.noreply.github.com>
2018-03-30 16:20:52 -0400

    task[5]: Implement onCreate and onDestroy method to unregister and register onSharedPreferenceListener

--- Lesson06-Visualizer-Preferences/T06.08-Exercise-PreferenceSummary/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
+++ Lesson06-Visualizer-Preferences/T06.08-Exercise-PreferenceSummary/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
@@ -82,8 +82,19 @@ public class SettingsFragment extends PreferenceFragmentCompat
     }
 
 
-    // TODO (5) Register and unregister the OnSharedPreferenceChange listener (this class) in
+    // COMP (5) Register and unregister the OnSharedPreferenceChange listener (this class) in
     // onCreate and onDestroy respectively.
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener
+                (this);
+    }
 }
