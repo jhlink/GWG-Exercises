d8fd35d41063a84d352d0772374ebd91c02c4c2c
James Kim <jhlink@users.noreply.github.com>
2018-03-30 23:36:10 -0400

    task[1]: Extend SQLitOpenHelper on the WaitlistDbHelper class

--- Lesson07-Waitlist/T07.02-Exercise-CreateTheDatabase/app/src/main/java/com/example/android/waitlist/data/WaitlistDbHelper.java
+++ Lesson07-Waitlist/T07.02-Exercise-CreateTheDatabase/app/src/main/java/com/example/android/waitlist/data/WaitlistDbHelper.java
@@ -1,7 +1,9 @@
 package com.example.android.waitlist.data;
 
-// TODO (1) extend the SQLiteOpenHelper class
-public class WaitlistDbHelper {
+import android.database.sqlite.SQLiteOpenHelper;
+
+// COMP (1) extend the SQLiteOpenHelper class
+public class WaitlistDbHelper extends SQLiteOpenHelper {
 
     // TODO (2) Create a static final String called DATABASE_NAME and set it to "waitlist.db"
 
