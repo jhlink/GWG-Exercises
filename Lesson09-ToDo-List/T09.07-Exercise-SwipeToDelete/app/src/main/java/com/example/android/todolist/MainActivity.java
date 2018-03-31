07c588f39118dbda39be3ff929f35a3377f7c914
James Kim <jhlink@users.noreply.github.com>
2018-03-31 17:46:34 -0400

    task[1-3]: Implement swipe action for delete

--- Lesson09-ToDo-List/T09.07-Exercise-SwipeToDelete/app/src/main/java/com/example/android/todolist/MainActivity.java
+++ Lesson09-ToDo-List/T09.07-Exercise-SwipeToDelete/app/src/main/java/com/example/android/todolist/MainActivity.java
@@ -18,6 +18,7 @@ package com.example.android.todolist;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.LoaderManager;
@@ -78,13 +79,21 @@ public class MainActivity extends AppCompatActivity implements
             public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
                 // Here is where you'll implement swipe to delete
 
-                // TODO (1) Construct the URI for the item to delete
+                // COMP (1) Construct the URI for the item to delete
+                int id = (int) viewHolder.itemView.getTag();
+                String stringId = Integer.toString(id);
+                Uri uri = TaskContract.TaskEntry.CONTENT_URI;
+                uri = uri.buildUpon().appendPath(stringId).build();
                 //[Hint] Use getTag (from the adapter code) to get the id of the swiped item
 
-                // TODO (2) Delete a single row of data using a ContentResolver
+                // COMP (2) Delete a single row of data using a ContentResolver
+                String selection = TaskContract.TaskEntry._ID + "=?";
+                String[] selectionArgs = new String[]{ stringId };
+                getContentResolver().delete(uri, selection, selectionArgs);
 
-                // TODO (3) Restart the loader to re-query for all tasks after a deletion
-                
+                // COMP (3) Restart the loader to re-query for all tasks after a deletion
+                getSupportLoaderManager().restartLoader(TASK_LOADER_ID, null, this);
+
             }
         }).attachToRecyclerView(mRecyclerView);
 
