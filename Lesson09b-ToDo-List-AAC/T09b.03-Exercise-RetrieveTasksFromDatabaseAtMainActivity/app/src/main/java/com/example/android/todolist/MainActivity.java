6bcc6468a351554b800df18b5ec5fe8b82ab791b
James Kim <jhlink@users.noreply.github.com>
2018-09-19 09:42:27 -0400

    task[1-2]: Create and initialize member variable for database in MainActivity

--- Lesson09b-ToDo-List-AAC/T09b.03-Exercise-RetrieveTasksFromDatabaseAtMainActivity/app/src/main/java/com/example/android/todolist/MainActivity.java
+++ Lesson09b-ToDo-List-AAC/T09b.03-Exercise-RetrieveTasksFromDatabaseAtMainActivity/app/src/main/java/com/example/android/todolist/MainActivity.java
@@ -26,6 +26,8 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.View;
 
+import com.example.android.todolist.database.AppDatabase;
+
 import static android.support.v7.widget.DividerItemDecoration.VERTICAL;
 
 
@@ -37,7 +39,8 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.ItemC
     private RecyclerView mRecyclerView;
     private TaskAdapter mAdapter;
 
-    // TODO (1) Create AppDatabase member variable for the Database
+    // DONE (1) Create AppDatabase member variable for the Database
+    private AppDatabase mDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -92,7 +95,8 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.ItemC
             }
         });
 
-        // TODO (2) Initialize member variable for the data base
+        // DONE (2) Initialize member variable for the data base
+        mDB = AppDatabase.getInstance(getApplicationContext());
     }
 
     /**
